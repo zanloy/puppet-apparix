5d788590f5448fea1a0fa489f1d864349dfc1011
Zan Loy <zan.loy@gmail.com>
2014-09-05 07:51:26 -0700

    updated formatting

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,30 +8,41 @@
 #
 class apparix {
 
-    File {
-        owner => 'root',
-        group => 'root',
-        mode => '0444',
-        backup => false,
-    }
+  File {
+    owner => 'root',
+    group => 'root',
+    mode => '0444',
+    backup => false,
+  }
 
-    file {
-        '/usr/bin/apparix':
-            ensure => file,
-            source => "puppet:///modules/${module_name}/usr/bin/apparix",
-            mode => '0755';
-        '/etc/bashrc.d/apparix':
-            ensure => file,
-            source => "puppet:///modules/${module_name}/etc/bashrc.d/apparix";
-        '/usr/share/doc/apparix':
-            ensure => directory,
-            source => "puppet:///modules/${module_name}/usr/share/doc/apparix",
-            recurse => true,
-            force => true,
-            purge => true;
-        '/usr/share/man/man1/apparix.1':
-            ensure => file,
-            source => "puppet:///modules/${module_name}/usr/share/man/man1/apparix.1";
+  case $::operatingsystem {
+    'Ubuntu' : {
+      package { 'apparix': ensure => latest }
     }
+    default : {
+      file {
+        '/usr/bin/apparix':
+          ensure => file,
+          source => "puppet:///modules/${module_name}/usr/bin/apparix",
+          mode => '0755';
+        '/usr/share/doc/apparix':
+          ensure => directory,
+          source => "puppet:///modules/${module_name}/usr/share/doc/apparix",
+          recurse => true,
+          force => true,
+          purge => true;
+        '/usr/share/man/man1/apparix.1':
+          ensure => file,
+          source => "puppet:///modules/${module_name}/usr/share/man/man1/apparix.1";
+      }
+    }
+  }
+
+    
+  file {
+    '/etc/bashrc.d/apparix':
+      ensure => file,
+      source => "puppet:///modules/${module_name}/etc/bashrc.d/apparix";
+  }
 
 }
